17137e5ea63db2f8d51a87f372899fbc3d889ddf
zwonn <57364353+zwonn@users.noreply.github.com>
2020-10-20 09:57:11 +0900

    1:1 chatting

--- Blahblah_t/src/main/java/com/gaon/prj/common/WebSocketHandler.java
+++ Blahblah_t/src/main/java/com/gaon/prj/common/WebSocketHandler.java
@@ -1,8 +1,11 @@
 package com.gaon.prj.common;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +15,30 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RequestMapping(value="/echo", method=RequestMethod.GET)
 public class WebSocketHandler extends TextWebSocketHandler {
 	// 세션 리스트
 	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
 	private Logger log = LoggerFactory.getLogger(WebSocketHandler.class);
+	
+	private List<WebSocketSession> users;
+	private Map<String, Object> userMap;
+	
+	ObjectMapper mapper = new ObjectMapper();
+	
+	public WebSocketHandler() {
+		users = new ArrayList<WebSocketSession>();
+		userMap = new HashMap<String, Object>();
+		
+	}
+	
 	// 클라이언트가 연결 되면 실행
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		sessionList.add(session);
+		users.add(session);
 		log.info("{} 연결됨" , session.getId());
 	}
 	 
@@ -29,10 +47,41 @@ public class WebSocketHandler extends TextWebSocketHandler {
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		log.info("{}로 부터 {} 받음", session.getId(), message.getPayload());
 		
+		
+		JSONObject object = mapper.readValue(message.getPayload(), JSONObject.class);
+		
+		String type = (String)object.get("type");
+		
+		
+		//등록
+		if(type.equals("register")) { 
+			System.out.println("등록");
+			String user = (String)object.get("userid");
+			userMap.put(user, session);
+		}
+		
+		//채팅
+		else if(type.equals("chat")){
+			System.out.println("서버에서 메시지보냄");
+			String target = (String)object.get("target");
+			WebSocketSession ws = (WebSocketSession)userMap.get(target);
+			System.out.println(ws);
+			String msg = (String)object.get("message");
+			
+			if(ws != null) {
+				ws.sendMessage(new TextMessage(msg));
+			}
+			else {
+				
+			}
+			
+		}
+		
+		else {
 		//모든 유저에게 메시지 출력
 		for(WebSocketSession sess : sessionList) {
-	
 			sess.sendMessage(new TextMessage(message.getPayload()));
+			}
 		}
 	}
 	
@@ -40,6 +89,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		sessionList.remove(session);
+		users.remove(session);
 		log.info("{} 연결 끊김.", session.getId());
 	}
 	
